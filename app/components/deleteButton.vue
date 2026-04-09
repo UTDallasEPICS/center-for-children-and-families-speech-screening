2388a3acd384ccc568bfa5cdc4db37f315f4bb8b
Thomas Nguyen <thomasnguyen5361@gmail.com>
2026-04-09 16:25:02 -0500

    Added componentization

---
+++ app/components/deleteButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="btn-danger" @click="handleClick">
+    <slot>Delete</slot>
+  </button>
+</template>
+
+<script setup>
+  const props = defineProps({
+    id: {
+      type: String,
+      required: true,
+    },
+  })
+
+  const emit = defineEmits(['confirm'])
+
+  function handleClick() {
+    const confirmed = confirm('Are you sure you want to delete this user?')
+    if (confirmed) {
+      emit('confirm', props.id)
+    }
+  }
+</script>
